0adaae496e8813030b87b8f0c8bd65c841e08389
Anuj Singh <editingwithanuj@gmail.com>
2024-02-11 19:48:05 +0530

    commit

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const connectdb = require("./connection");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -10,8 +9,6 @@ app.use(express.json());
 app.get("/", async (req, res) => {
   res.send("api is running............");
 });
-
-connectdb();
 
 app.listen(PORT, () => {
   console.log(`server is connected and live on port ${PORT}`);
